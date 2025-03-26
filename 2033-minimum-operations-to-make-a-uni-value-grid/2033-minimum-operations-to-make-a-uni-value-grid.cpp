dce26b4a906f37ecb55a845855ffc46410c2f9a7
Omkar Kale <130669802+omkarkale2810@users.noreply.github.com>
2025-03-26 23:58:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int> arr;
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        for(int i = 0 ; i < n  ; i++){
+            for(int j = 0 ; j < m ; j ++){
+                arr.push_back(grid[i][j]);
+            }
+        }
+        int l = m+n;
+        sort(arr.begin() , arr.end() );
+        int target = arr[(l/2)];
+        int c = 0;
+
+        for(int i = 0 ; i < l ; i++){
+            if(arr[i] % x  != target % x ){
+                return -1;
+            }
+            c += abs(target - arr[i])/x ;
+        }
+
+        return c;
+
+
+    }
+};
