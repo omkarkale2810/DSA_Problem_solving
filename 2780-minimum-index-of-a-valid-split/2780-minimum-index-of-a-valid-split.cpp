b2d722d56ca22090025af6432fc61813fddf5db8
Omkar Kale <130669802+omkarkale2810@users.noreply.github.com>
2025-03-27 23:16:48 +0530

    Time: 57 ms (23.36%), Space: 102.9 MB (19.26%) - LeetHub

---
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> mp1;
+        unordered_map<int,int> mp2;
+        for(int i = 0 ;i < n ; i++){
+            mp2[nums[i]]++;
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            mp1[nums[i]]++;
+            if(mp2[nums[i]]){
+                mp2[nums[i]]--;
+            }
+            int n1 = i+1;
+            int n2 = n-i-1;
+            if( (mp1[nums[i]] > n1/2) && (mp2[nums[i]] > n2/2) ){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
