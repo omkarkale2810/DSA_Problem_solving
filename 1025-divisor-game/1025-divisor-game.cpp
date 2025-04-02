2de05d252c8b1f071be253626b117fb79de92ac0
Omkar Kale <130669802+omkarkale2810@users.noreply.github.com>
2025-04-02 16:30:56 +0530

    Time: 0 ms (100%), Space: 8.3 MB (13.57%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
+    bool solve(int n , bool flag ){
+        if( n == 1 && flag){
+            return true; 
+        }
+        for(int i = 1 ; i < n ; i++){
+            if(n%i == 0){
+                return solve( n-i , !flag);
+            }
+        }
+        return false;
+    }
     bool divisorGame(int n) {
-       return n%2 == 0; 
+    //    return n%2 == 0;
+        bool flag = 1;
+        vector<bool> dp(n+1,0);
+        return !solve( n , 1 );
+        
+
+
     }
 };
