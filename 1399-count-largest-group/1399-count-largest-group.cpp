53ce9e449c697cc09e47ab5540d1f47f7db02f6d
Omkar Kale <130669802+omkarkale2810@users.noreply.github.com>
2025-04-24 10:34:43 +0530

    Time: 2 ms (48.3%), Space: 8.1 MB (44.22%) - LeetHub

---
+++ 1399-count-largest-group/1399-count-largest-group.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int countdigitsum(int n){
+        int sum = 0;
+        while(n > 0 ){
+            int d = n%10;
+            sum+=d;
+            n = n/10;
+        }
+        return sum;
+    }
+    int countLargestGroup(int n) {
+        
+        unordered_map<int,int> mp;
+        int maxsize = 0;
+        int c =0;
+
+        for(int i = 1 ; i <= n ; i++){
+            int sum = 0;
+            sum = countdigitsum(i);
+            mp[sum]++;
+
+            if(mp[sum] == maxsize){
+                c++;
+            }
+            else if(mp[sum] > maxsize){
+                maxsize = mp[sum];
+                c = 1;
+            }
+        }
+        return c;
+    }
+};
