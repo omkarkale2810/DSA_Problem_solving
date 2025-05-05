47e57891cdd6f2dbeb5384cde55dcb840639a9b4
Omkar Kale <130669802+omkarkale2810@users.noreply.github.com>
2025-05-06 00:49:24 +0530

    Time: 0 ms (100%), Space: 9 MB (55.01%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -15,6 +15,19 @@ public:
     }
     int numTilings(int n) {
         vector<int> dp(n+1,-1);
-        return solve(n  , dp);
+        // return solve(n  , dp);
+        if(n == 1 || n == 2){
+            return n;
+        }
+        if( n == 3){
+            return 5;
+        }
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+        for(int i = 4 ; i<=n ; i++){
+            dp[i] = ((2*dp[i-1])%M + (dp[i-3]%M))%M;
+        }
+        return dp[n];
     }
 };
