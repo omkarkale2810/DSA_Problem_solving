a848887781261e0e767db3794b28fe737710d864
Omkar Kale <130669802+omkarkale2810@users.noreply.github.com>
2025-05-20 16:07:29 +0530

    Time: 77 ms (36.39%), Space: 14 MB (28.82%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(int n , vector<int>& dp){
+        if( n == 0 ){
+            return 0;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        int result = INT_MAX;
+        for(int i = 1 ; i*i <= n ; i++){
+            int take = 1 + solve(n-i*i , dp);
+            result = min(take , result);
+        }
+        return dp[n] = result;
+
+    }
+    int numSquares(int n) {
+        vector<int> dp(n+1 , -1);
+        return solve(n , dp);
+    }
+};
