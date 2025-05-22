e7f850af7a8a568401dbf4c08ecfdf6dbb377e1f
Omkar Kale <130669802+omkarkale2810@users.noreply.github.com>
2025-05-22 12:43:53 +0530

    Time: 3 ms (81.46%), Space: 14.1 MB (78.86%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -17,7 +17,22 @@ public:
 
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return solve(0, -1, nums, n, dp);
+        // vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        // return solve(0, -1, nums, n, dp);
+
+        // new method patience sorting
+        vector<int> sorted;
+
+        for(int i = 0 ; i < n ; i++){
+            auto it = lower_bound(sorted.begin() , sorted.end() , nums[i]);
+
+            if(it == end(sorted)){
+                sorted.push_back(nums[i]);
+            }
+            else{
+                *it = nums[i];
+            }
+        }
+        return sorted.size();
     }
 };
