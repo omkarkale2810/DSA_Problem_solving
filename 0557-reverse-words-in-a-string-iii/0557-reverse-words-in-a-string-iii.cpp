cce498147b74f3ef12731410499ebe8435a8fe98
Omkar Kale <130669802+omkarkale2810@users.noreply.github.com>
2025-06-01 15:31:52 +0530

    Time: 6 ms (32.79%), Space: 16.5 MB (20.9%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+
+        string result = "";
+        string token = "";
+
+        while(ss >> token) {
+            reverse(begin(token), end(token));
+            result += token + " ";
+        }
+
+        return result.substr(0, result.length()-1);
+
+    }
+};
