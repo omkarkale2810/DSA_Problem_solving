79133cb6564e328c9d117f019ef44e11258020d3
Omkar Kale <130669802+omkarkale2810@users.noreply.github.com>
2025-06-24 19:06:30 +0530

    Time: 3 ms (72.14%), Space: 13.5 MB (75.33%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    void solve(string& curr, int n, int open, int close){
+        
+        if(curr.length() == 2*n){
+            ans.push_back(curr);
+            return;
+        }
+        if(open < n){
+            curr.push_back('(');
+            solve(curr,n,open+1,close);
+            curr.pop_back();
+        }
+        if(open > close){
+            curr.push_back(')');
+            solve(curr,n,open,close+1);
+            curr.pop_back();
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        string curr = "";
+        solve(curr , n , 0 , 0);
+        return ans;    
+    }
+};
