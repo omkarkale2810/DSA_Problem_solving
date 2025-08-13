6519ceaa92e0da3461b472e6b926b803830289ef
Omkar Kale <130669802+omkarkale2810@users.noreply.github.com>
2025-08-13 13:17:18 +0530

    Time: 0 ms (100%), Space: 8.8 MB (97.26%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool solve(int n){
+        if(n == 1){
+            return true;
+        }
+        if(n < 0){
+            return false;
+        }
+        if(n%3 != 0){
+            return false;
+        }
+        return solve(n/3);
+    }
+    bool isPowerOfThree(int n) {
+        if(n <= 0){
+            return false;
+        }
+        return solve(n);
+
+    }
+};
