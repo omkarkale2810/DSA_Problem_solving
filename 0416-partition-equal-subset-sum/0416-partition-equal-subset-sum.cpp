ff95b8102858857cf07b443b6721b340df6dbd26
Omkar Kale <130669802+omkarkale2810@users.noreply.github.com>
2025-10-02 15:54:06 +0530

    Time: 146 ms (40.37%), Space: 17.5 MB (61.03%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -23,7 +23,27 @@ public:
         }
         int target = s / 2;
         int n = nums.size();
-        vector<vector<int>> dp(n , vector<int>(target + 1 , -1));
-        return solve(target , 0 , nums , n , dp);
+        // vector<vector<int>> dp(n , vector<int>(target + 1 , -1));
+        // return solve(target , 0 , nums , n , dp);
+        
+        vector<vector<bool>> dp(n , vector<bool>(target + 1 , false));
+        for(int i = 0 ; i < n ; i++){
+            dp[i][0] = true;
+        }
+
+        if(nums[0] <= target) dp[0][nums[0]] = true;
+
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 1; j <= target ; j++){
+                bool not_take = dp[i-1][j];
+                bool take = false;
+                if(j >= nums[i]){
+                    take = dp[i-1][j-nums[i]];
+                }
+                dp[i][j] = take || not_take;
+            }
+        }
+
+        return dp[n-1][target];
     }
 };
